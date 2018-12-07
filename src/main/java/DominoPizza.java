c9539f1c7b17528c0d36f8487f064a35b79f8147
Umons-170961 <44571356+Umons-170961@users.noreply.github.com>
2018-12-07 15:15:14 +0100

    15:15

--- src/main/java/DominoPizza.java
+++ src/main/java/DominoPizza.java
@@ -1,11 +1,23 @@
 public class DominoPizza implements Pizzeria{
     static DominoPizza Singleton;
+    private State state;
+
 
     private DominoPizza(){
         Singleton = this;
+        this.state = new EnService(this);
     }
 
-    public DominoPizza getDominoPizza(){
+    public void changeState(State state){
+        this.state = state;
+    }
+
+    public State getState(){
+        return state;
+    }
+
+    @Override
+    public DominoPizza createPizzeria(){
         if (Singleton == null){
             new DominoPizza();
         }
--- src/main/java/EnService.java
+++ src/main/java/EnService.java
@@ -1,3 +1,8 @@
 public class EnService extends State{
+    EnService(Pizzeria pizzeria){
+        super(pizzeria);
+    }
+
+    public int test(){return 0;}
 
 }
