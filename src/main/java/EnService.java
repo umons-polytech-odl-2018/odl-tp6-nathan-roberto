5e605c6219c1ddd8f9802ac486209d45c7d74f6a
Umons-170961 <44571356+Umons-170961@users.noreply.github.com>
2018-12-07 14:45:36 +0100

    14:45

---
+++ src/main/java/EnService.java
@@ -0,0 +1,3 @@
+public class EnService extends State{
+
+}
--- src/main/java/PizzaHut.java
+++ src/main/java/PizzaHut.java
@@ -1,2 +1,15 @@
-public class PizzaHut {
+public class PizzaHut implements Pizzeria{
+        static PizzaHut Singleton;
+
+        private PizzaHut(){
+            Singleton = this;
+        }
+
+        public PizzaHut getPizzaHut(){
+            if (Singleton == null){
+                new PizzaHut();
+            }
+            return Singleton;
+        }
 }
+
