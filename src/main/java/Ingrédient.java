e37b5c3fa8f0313c42c8440094f35919d3fce2cb
Rreboot8x5a <171399@student.umons.ac.be>
2018-12-07 13:59:43 +0100

    début

---
+++ src/main/java/Ingrédient.java
@@ -0,0 +1,4 @@
+public class Ingrédient implements nourriture{
+    @Override
+    public float getPrice(){return 11;}
+}
---
+++ src/main/java/Pizza.java
@@ -0,0 +1,4 @@
+public class Pizza implements nourriture {
+    @Override
+    public float getPrice(){return 23;}
+}
