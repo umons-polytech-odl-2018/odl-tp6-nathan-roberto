feae918102ef5eb9ecc6d5daebb239af91b057e5
Rreboot8x5a <171399@student.umons.ac.be>
2018-12-07 14:32:46 +0100

    début

--- src/main/java/Pizza.java
+++ src/main/java/Pizza.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+
+
 public class Pizza implements nourriture {
     protected float price;
     protected String name;
-
+    //protected ArrayList<Ingrédient> Recette;
+    ArrayList<Ingrédient> Recette = new ArrayList<Ingrédient>();
     @Override
     public float getPrice(){return price;}
 
+    public void addIngrédient(Ingrédient ingrédient){
+        Recette.add(ingrédient);
+    }
 
 }
